466b78ee718884726f74f66e1cf845f298969448
yangzhao_kl <yangzhao1@kylinos.cn>
2021-01-19 10:42:57 +0800

    !107 fix run command with pipeline * fix run command with pipeline

--- utils/command.go
+++ utils/command.go
@@ -2,26 +2,13 @@ package utils
 
 import (
 	"os/exec"
-	"strings"
-
-	"errors"
 
 	"github.com/beego/beego/v2/core/logs"
 )
 
 // RunCommand runs the command and get the result
 func RunCommand(c string) ([]byte, error) {
-	// cmd := exec.Command(c)
-	// return cmd.CombinedOutput()
-	c = strings.Trim(c, " ")
-	index := strings.Index(c, " ")
-	if index < 0 {
-		return nil, errors.New("invalid command")
-	}
-
-	cmd := c[0:index]
-	parameter := c[index:]
 	logs.Debug("Running command: %s", c)
-	command := exec.Command(cmd, parameter)
+	command := exec.Command("bash", "-c", c)
 	return command.CombinedOutput()
 }
