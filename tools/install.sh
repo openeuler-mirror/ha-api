e69f12d27cc3118280dac535df92f4155389ef5b
hey_woods <zhanyige@kylinos.cn>
2021-07-07 06:57:13 +0000

    !161 #I3Y4HM 在安装前进行路径检查 * check ha-api path

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,8 @@
 # install bin and static files
+# check path
+if [ ! -d "/usr/share/heartbeat-gui/ha-api" ]; then
+  mkdir -p /usr/share/heartbeat-gui/ha-api
+fi
 cp ../ha-api /usr/share/heartbeat-gui/ha-api/
 chmod +x /usr/share/heartbeat-gui/ha-api/ha-api
 cp -r ../views /usr/share/heartbeat-gui/ha-api/
