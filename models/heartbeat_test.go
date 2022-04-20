174fccdee4d2b42103b69dcbdb1320d574991fa6
liqiuyu <liqiuyu@kylinos.cn>
2022-04-20 14:21:53 +0800

    add file comments

--- models/heartbeat_test.go
+++ models/heartbeat_test.go
@@ -1,3 +1,17 @@
+/******************************************************************************
+ * Copyright (c) KylinSoft Co., Ltd.2021-2022. All rights reserved.
+ * ha-api is licensed under the Mulan PSL v2.
+ * You can use this software accodring to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: liqiuyu
+ * Date: 2022-04-19 16:49:51
+ * LastEditTime: 2022-04-20 10:30:21
+ * Description: 磁盘心跳测试用例
+ ******************************************************************************/
 package models
 
 import (
