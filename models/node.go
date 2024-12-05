a4ec907fa4b1907f451f674ad32fa3113afbea58
bizhiyuan <bizhiyuan@kylinos.cn>
2024-12-05 14:12:33 +0800

    Update fields for Get interface of Node

--- models/node.go
+++ models/node.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) KylinSoft  Co., Ltd. 2024.All rights reserved.
- * ha-api licensed under the Mulan Permissive Software License, Version 2. 
+ * ha-api licensed under the Mulan Permissive Software License, Version 2.
  * See LICENSE file for more details.
  * Author: yangzhao_kl <yangzhao1@kylinos.cn>
  * Date: Fri Jan 8 20:56:40 2021 +0800
