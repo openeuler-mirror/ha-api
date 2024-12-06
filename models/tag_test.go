99539f4d907773c58b1404b55cbb9e2009fdf24e
bixiaoyan <bixiaoyan@kylinos.cn>
2024-12-06 16:27:42 +0800

    Add SetTag unittest case

--- models/tag_test.go
+++ models/tag_test.go
@@ -9,12 +9,39 @@
 package models
 
 import (
+	"encoding/json"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetTag(t *testing.T) {
 	result := GetTag()
 	if result.Action != true {
 		t.Fatal("Get tag failed")
+	}
+}
+
+func TestSetTag(t *testing.T) {
+	testCases := []struct {
+		input    []byte
+		expected map[string]interface{}
+	}{
+		{
+			[]byte(`{"id":"tag1","tag_resource": ["res1"]}`),
+			map[string]interface{}{"action": true, "info": "Add tag success"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		result := SetTag(testCase.input)
+		resultJson, err := json.Marshal(result)
+		require.NoError(t, err, "Marshal not return an error")
+
+		expectedJson, err := json.Marshal(testCase.expected)
+		require.NoError(t, err, "Marshal expected map not return an error")
+
+		assert.JSONEq(t, string(expectedJson), string(resultJson), "Add tag success")
 	}
 }
