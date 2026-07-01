c81c2dfe882bab6cb711b5ffa760cd55718ab4e3
bizhiyuan <bizhiyuan@kylinos.cn>
2026-07-01 15:02:42 +0800

    Optimize the log module

--- controllers/log.go
+++ controllers/log.go
@@ -8,14 +8,20 @@
 package controllers
 
 import (
+	"fmt"
+	"io"
+	"log/slog"
 	"net/http"
+	"os"
 	"path/filepath"
-        "strings"
+	"strings"
 
-	"github.com/beego/beego/v2/core/logs"
 	"github.com/beego/beego/v2/server/web"
+	"github.com/chai2010/gettext-go"
 
 	"gitee.com/openeuler/ha-api/models"
+	"gitee.com/openeuler/ha-api/settings"
+	"gitee.com/openeuler/ha-api/utils"
 )
 
 type LogController struct {
@@ -23,20 +29,43 @@ type LogController struct {
 }
 
 func (lc *LogController) Get() {
-	logs.Debug("handle resource GET request")
-	result := models.GenerateLog()
-	lc.Data["json"] = &result
-	lc.ServeJSON()
+	slog.Debug("handle resource GET request")
+        // todo: multi clusters 
+	result, geterr := models.GenerateLog()
+
+	if geterr != nil {
+		res := map[string]interface{}{}
+		res["action"] = false
+		res["error"] = geterr.Error()
+		lc.Data["json"] = &res
+		lc.ServeJSON()
+	} else {
+
+		FileInfo, err := os.Stat(result)
+		if err != nil {
+			res := map[string]interface{}{}
+			res["action"] = false
+			res["error"] = gettext.Gettext(fmt.Sprintf("Can not stat file %s", result))
+			slog.Error("Can not stat file", "file", result, "error", err)
+			lc.Data["json"] = &res
+			lc.ServeJSON()
+			return
+		}
+		slog.Info("Generated log file", "file", result)
+		defer os.Remove(result)
+		lc.Ctx.Output.Download(result, FileInfo.Name())
+	}
+
 }
 
 func (lc *LogController) Put() {
-	logs.Debug("handle resource PUT request")
+	slog.Debug("handle resource PUT request")
 
 	lc.ServeJSON()
 }
 
 func (lc *LogController) Post() {
-	logs.Debug("handle resource POST request")
+	slog.Debug("handle resource POST request")
 
 	lc.ServeJSON()
 }
@@ -46,27 +75,26 @@ type LogDownloadController struct {
 }
 
 func (ldc *LogDownloadController) Get() {
-        logs.Debug("handle log download GET request")
-        fileTail := ldc.Ctx.Input.Param(":filetail")
-        // Prevent path traversal: extract only the base filename
-        fileTail = filepath.Base(fileTail)
-        // Validate file extension
-        if !strings.HasSuffix(fileTail, ".tar") {
-                ldc.Ctx.Output.SetStatus(http.StatusBadRequest)
-                ldc.Data["json"] = map[string]interface{}{"action": false, "error": "invalid filename"}
-                ldc.ServeJSON()
-                return
-        }
+	slog.Debug("handle log download GET request")
+	fileTail := ldc.Ctx.Input.Param(":filetail")
+	// Prevent path traversal: extract only the base filename
+	fileTail = filepath.Base(fileTail)
+	// Validate file extension
+	if !strings.HasSuffix(fileTail, ".tar") {
+		ldc.Ctx.Output.SetStatus(http.StatusBadRequest)
+		ldc.Data["json"] = map[string]interface{}{"action": false, "error": "invalid filename"}
+		ldc.ServeJSON()
+		return
+	}
 
-        const filePath = "/usr/share/heartbeat-gui/ha-api/static/"
-        filePrefix := "kylinha-log-"
-        fullPath := filepath.Join(filePath, filePrefix+fileTail)
-        // Verify the resolved path is still within the expected directory
-        if !strings.HasPrefix(fullPath, filePath) {
-                ldc.Ctx.Output.SetStatus(http.StatusBadRequest)
-                ldc.Data["json"] = map[string]interface{}{"action": false, "error": "invalid filename"}
-                ldc.ServeJSON()
-                return
-        }
-        http.ServeFile(ldc.Ctx.ResponseWriter, ldc.Ctx.Request, fullPath)
+	filePrefix := "kylinha-log-"
+	fullPath := filepath.Join(settings.StaticDir, filePrefix+fileTail)
+	// Verify the resolved path is still within the expected directory
+	if !strings.HasPrefix(fullPath, settings.StaticDir) {
+		ldc.Ctx.Output.SetStatus(http.StatusBadRequest)
+		ldc.Data["json"] = map[string]interface{}{"action": false, "error": "invalid filename"}
+		ldc.ServeJSON()
+		return
+	}
+	http.ServeFile(ldc.Ctx.ResponseWriter, ldc.Ctx.Request, fullPath)
 }
