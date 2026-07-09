f911b307a7be6fd7456851d49b07bb9c18c302d4
bizhiyuan <bizhiyuan@kylinos.cn>
2026-07-09 19:18:57 +0800

    Refactor: add common cmd

--- utils/command_test.go
+++ utils/command_test.go
@@ -1,10 +1,11 @@
 /*
  * Copyright (c) KylinSoft  Co., Ltd. 2024.All rights reserved.
- * ha-api licensed under the Mulan Permissive Software License, Version 2. 
+ * ha-api licensed under the Mulan Permissive Software License, Version 2.
  * See LICENSE file for more details.
- * Author: yangzhao_kl <yangzhao1@kylinos.cn>
- * Date: Fri Jan 22 17:27:48 2021 +0800
+ * Author: bixiaoyan <bixiaoyan@kylinos.cn>
+ * Date: Thu Mar 27 09:32:28 2025 +0800
  */
+
 package utils
 
 import (
